70321e66f1c52ee8b1ab6ea71bc72d6cbe8abd7c
marosandor <marosandor2001@gmail.com>
2022-12-07 09:03:52 +0100

    style

--- components/Opinions.js
+++ components/Opinions.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Button, StyleSheet, View, Text } from 'react-native';
+import React, { Component } from "react";
+import { Button, StyleSheet, View, Text } from "react-native";
 
 export default class Opinions extends Component {
   constructor(props) {
@@ -7,15 +7,15 @@ export default class Opinions extends Component {
 
     this.state = {
       velemenyek: [],
-      isLoading: true
+      isLoading: true,
     };
   }
 
   async velemenyekLekeres() {
     try {
-      const response = await fetch('http://192.168.154.97:24001/jaratok');
+      const response = await fetch("http://192.168.154.97:24001/velemenyek");
       const json = await response.json();
-      console.log(json)
+      console.log(json);
       this.setState({ velemenyek: json });
     } catch (error) {
       console.log("Hiba:", error);
@@ -25,26 +25,82 @@ export default class Opinions extends Component {
   }
 
   componentDidMount() {
-    this.velemenyekLekeres()
+    this.velemenyekLekeres();
   }
 
-  render() {    
+  render() {
     return (
-      <View style={styles.container}>
-        {this.state.velemenyek.map((elem) =>
-          <Text key={elem.route_short_name} style={styles.text}>{elem}</Text>
-        )}
+      <View style={styles.main_container}>
+        {this.state.velemenyek.map((elem) => (
+          <View style={styles.container}>
+            <View style={styles.jarat_box}>
+              <Text sytle={styles.jarat_szam} key={elem.route_short_name}>
+                {elem.route_short_name}
+              </Text>
+            </View>
+            <View style={styles.jarat_velemeny}>
+              <View style={styles.comfort}>
+                <Text sytle={styles.text} key={elem.route_short_name}>
+                  Kényelem: {elem.comfort}
+                </Text>
+              </View>
+              <View style={styles.comfort}>
+                <Text sytle={styles.text} key={elem.route_short_name}>
+                  Forgalom: {elem.crowd}
+                </Text>
+              </View>
+              <View style={styles.comfort}>
+                <Text sytle={styles.text} key={elem.route_short_name}>
+                  Idő: {elem.time}
+                </Text>
+              </View>
+            </View>
+          </View>
+          // <View style={styles.containers}>
+          //   <View style={{ flex: 1, padding: 20 }}>
+          //     <Text sytle={styles.text} key={elem.route_short_name}>{elem.route_short_name}</Text>
+          //   </View>
+          // </View>
+        ))}
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  main_container: {
+    margin: 5,
+    padding: 10,
+    borderRadius: 15,
+    borderWidth: 1,
     flex: 1,
   },
 
+  container: {
+    flexDirection: "row",
+    padding: 5,
+    height: 75
+  },
+
+  jarat_box: {
+    flex: 1,
+    backgroundColor: "red",
+    justifyContent: 'center'
+  },
+
+  jarat_velemeny: {
+    flex: 4,
+    backgroundColor: "darkorange",
+    justifyContent: 'center'
+  },
+
+  jarat_szam: {
+    textAlign: 'center'
+  },
+
   text: {
-    color: 'black'
-  }
+    color: "black",
+    justifyContent: 'center',
+    textAlign: 'center'
+  },
 });
