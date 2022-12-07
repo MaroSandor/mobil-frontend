5740b33ac09b446860fa4de37c68d0256c8fda7e
marosandor <marosandor2001@gmail.com>
2022-12-07 11:18:45 +0100

    style

--- components/Opinions.js
+++ components/Opinions.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Button, StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, ScrollView, TouchableOpacity, DevSettings } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export default class Opinions extends Component {
   constructor(props) {
@@ -30,39 +31,39 @@ export default class Opinions extends Component {
 
   render() {
     return (
+      <ScrollView>
       <View style={styles.main_container}>
         {this.state.velemenyek.map((elem) => (
           <View style={styles.container}>
             <View style={styles.jarat_box}>
-              <Text sytle={styles.jarat_szam} key={elem.route_short_name}>
+              <Text style={styles.jarat_szam} key={elem.route_short_name.toString()}>
                 {elem.route_short_name}
               </Text>
             </View>
             <View style={styles.jarat_velemeny}>
               <View style={styles.comfort}>
-                <Text sytle={styles.text} key={elem.route_short_name}>
-                  Kényelem: {elem.comfort}
+                <Text style={styles.text} key={elem.route_short_name.toString()}>
+                  <Text style={{ color: 'red' }}>Kényelem: </Text>{elem.comfort}
                 </Text>
               </View>
               <View style={styles.comfort}>
-                <Text sytle={styles.text} key={elem.route_short_name}>
-                  Forgalom: {elem.crowd}
+                <Text style={styles.text} key={elem.route_short_name.toString()}>
+                  <Text style={{ color: 'red' }}>Forgalom: </Text>{elem.crowd}
                 </Text>
               </View>
               <View style={styles.comfort}>
-                <Text sytle={styles.text} key={elem.route_short_name}>
-                  Idő: {elem.time}
+                <Text style={styles.text} key={elem.route_short_name.toString()}>
+                  <Text style={{ color: 'red' }}>Idő: </Text>{elem.ido}
                 </Text>
               </View>
             </View>
+            <View style={styles.jarat_comment}>
+              <MaterialIcons style={styles.icon} name="arrow-right" />
+            </View>
           </View>
-          // <View style={styles.containers}>
-          //   <View style={{ flex: 1, padding: 20 }}>
-          //     <Text sytle={styles.text} key={elem.route_short_name}>{elem.route_short_name}</Text>
-          //   </View>
-          // </View>
         ))}
       </View>
+      </ScrollView>
     );
   }
 }
@@ -71,36 +72,55 @@ const styles = StyleSheet.create({
   main_container: {
     margin: 5,
     padding: 10,
-    borderRadius: 15,
-    borderWidth: 1,
-    flex: 1,
+    flex: 2,
   },
 
   container: {
     flexDirection: "row",
     padding: 5,
-    height: 75
+    height: 100,
   },
 
   jarat_box: {
-    flex: 1,
-    backgroundColor: "red",
-    justifyContent: 'center'
+    flex: 2,
+    backgroundColor: "#555",
+    justifyContent: 'center',
+    borderTopLeftRadius: 15,
+    borderBottomLeftRadius: 15,
   },
 
   jarat_velemeny: {
-    flex: 4,
-    backgroundColor: "darkorange",
-    justifyContent: 'center'
+    flex: 5,
+    backgroundColor: "#a1a1a1",
+    justifyContent: 'center',
+    paddingLeft: 15
   },
 
   jarat_szam: {
-    textAlign: 'center'
+    fontSize: 25,
+    fontWeight: 'bold',
+    textAlign: "center",
+    color: "white",
   },
 
   text: {
-    color: "black",
-    justifyContent: 'center',
-    textAlign: 'center'
+    color: "white",
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 15
   },
+
+  jarat_comment: {
+    flex: 2,
+    backgroundColor: "#555",
+    justifyContent: "center",
+    alignItems: "center",
+    borderTopRightRadius: 15,
+    borderBottomRightRadius: 15,
+  },
+
+  icon: {
+    fontSize: 50,
+    color: 'white'
+  }
 });
